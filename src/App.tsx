eda29b86e96b2350587a72cd6f3ae6ffb22cca57
zakiah <zakiah_bloom@yahoo.com>
2023-09-18 16:30:32 +0800

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hellow</div>;
+  return (
+    <div className="App">
+      Todo-List Step
+      <p>
+        Hello there! Welcome to my project, where we will embark on a journey to
+        build a Todo List application right from scratch and how you can improve
+        and make your code even better using handy libraries. Let's get started!
+      </p>
+    </div>
+  );
 }
 
 export default App;
